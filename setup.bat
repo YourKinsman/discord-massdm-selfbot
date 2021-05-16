6b8f8f1494cffe5948f6e6a94de021e2a86359a5
Kinsman <83188114+YourKinsman@users.noreply.github.com>
2021-05-16 13:11:38 +0100

    Setup file to make sure everything works.

---
+++ setup.bat
@@ -0,0 +1,11 @@
+pip install discord
+pip install os
+pip install colorama
+pip install requests
+pip install time
+pip install termcolor
+pip install dotenv
+
+cls
+echo Everythings finished. You should hopefully be able to run "message.py" now!
+pause
